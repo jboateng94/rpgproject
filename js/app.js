67132a1f59167358af1ef93bda2de899d20bdd50
Jeremy Boateng <jeremyba@spartaglobal.co>
2017-01-30 15:34:23 +0000

    started menu for magic (will come back to this mechanic later)

--- js/app.js
+++ js/app.js
@@ -2,31 +2,44 @@
 $(document).foundation();
 
 var playerHP = 100;
+var playerMP = 100;
+
 var enemyHP = 100;
+var enemyMP = 50;
+
 var messageBox = $("textarea#messagebox");
 
 var fightTest = $( "#fight" );
 
-var enemyOptions = [];
+//var playerMagicOptions = {"Glitch slap":20, "Oh damn":30};
 
 
 function updateMessage(arg) {
 	messageBox.html(arg);
 }
 
-updateMessage("Enemy health:"+enemyHP);
 
-fightTest.on('click', playerMove);
 
-function playerMove(){
-	enemyHP -= 10;
+function buttonInitialisers() {
+	fightTest.on('click', playerAttack);
+}
+
+updateMessage("Player health: "+playerHP+"	Enemy health: "+enemyHP);
+
+buttonInitialisers();
+
+//Player physical attack logic
+function playerAttack(){
+	enemyHP -= 20;
 	if(enemyHP <= 0){
 		alert("You win!")
 	}
-	updateMessage("Enemy health:"+enemyHP);
+	updateMessage("Enemy health: "+enemyHP + "   Player health: "+playerHP);
 	console.log("Enemy health:"+enemyHP);
-	//setTimeout(enemyMove(), 3000);
+	enemyMove();
 }
+
+
 
 function enemyMove() {
 	fightTest.off('click');
@@ -34,7 +47,7 @@ function enemyMove() {
 	if(playerHP <= 0){
 		alert("You lose")
 	}
-	updateMessage("Player health:"+playerHP);
+	updateMessage("Player health: "+playerHP+"	Enemy health: "+enemyHP);
 	console.log("Player health:"+playerHP);
-	fightTest.on('click', playerMove);
+	buttonInitialisers();
 }
