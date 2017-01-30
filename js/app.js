cd2219b4ec3f212caa3891c42cb2fb1761e523f3
Jeremy Boateng <jeremyba@spartaglobal.co>
2017-01-30 13:45:25 +0000

    finished basic functionality for fight

--- js/app.js
+++ js/app.js
@@ -1,17 +1,40 @@
-$( document ).ready(function() {
-  // Handler for .ready() called.
 
-  $(document).foundation();
+$(document).foundation();
 
-  var playerHP = 1000;
-  var enemyHP = 100;
-  var messageBox = $("textarea#messagebox");
-  // messageBox.html(playerHP);
-  
-  function updateMessage(arg) {
-  	messageBox.html(arg);
-  }
+var playerHP = 100;
+var enemyHP = 100;
+var messageBox = $("textarea#messagebox");
 
-  updateMessage("test");
+var fightTest = $( "#fight" );
 
-});
+var enemyOptions = [];
+
+
+function updateMessage(arg) {
+	messageBox.html(arg);
+}
+
+updateMessage("Enemy health:"+enemyHP);
+
+fightTest.on('click', playerMove);
+
+function playerMove(){
+	enemyHP -= 10;
+	if(enemyHP <= 0){
+		alert("You win!")
+	}
+	updateMessage("Enemy health:"+enemyHP);
+	console.log("Enemy health:"+enemyHP);
+	//setTimeout(enemyMove(), 3000);
+}
+
+function enemyMove() {
+	fightTest.off('click');
+	playerHP -= 15;
+	if(playerHP <= 0){
+		alert("You lose")
+	}
+	updateMessage("Player health:"+playerHP);
+	console.log("Player health:"+playerHP);
+	fightTest.on('click', playerMove);
+}
