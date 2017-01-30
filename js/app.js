8dcff6dd2565f83defa521345c8579c4951d00fa
Jeremy Boateng <jeremyba@spartaglobal.co>
2017-01-30 12:00:55 +0000

    started writing functionality for fight option in js and made function to edit messagebar

--- js/app.js
+++ js/app.js
@@ -1,2 +1,17 @@
-$(document).foundation();
+$( document ).ready(function() {
+  // Handler for .ready() called.
 
+  $(document).foundation();
+
+  var playerHP = 1000;
+  var enemyHP = 100;
+  var messageBox = $("textarea#messagebox");
+  // messageBox.html(playerHP);
+  
+  function updateMessage(arg) {
+  	messageBox.html(arg);
+  }
+
+  updateMessage("test");
+
+});
