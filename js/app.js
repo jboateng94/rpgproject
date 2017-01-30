8f14b4d498d97b4c734708d561529c31e0f92976
Jeremy Boateng <jeremyba@spartaglobal.co>
2017-01-30 16:14:07 +0000

    finished guard mechanic

--- js/app.js
+++ js/app.js
@@ -3,13 +3,18 @@ $(document).foundation();
 
 var playerHP = 100;
 var playerMP = 100;
+var playerDmg = 10;
+var checkPlayerGuard = false;
 
 var enemyHP = 100;
 var enemyMP = 50;
 
+var enemyDmg = 10;
+
 var messageBox = $("textarea#messagebox");
 
 var fightTest = $( "#fight" );
+var guardTest = $( "#guard" );
 
 //var playerMagicOptions = {"Glitch slap":20, "Oh damn":30};
 
@@ -19,18 +24,30 @@ function updateMessage(arg) {
 }
 
 
-
 function buttonInitialisers() {
 	fightTest.on('click', playerAttack);
+	guardTest.on('click', function() {
+		if(checkPlayerGuard === false){
+			checkPlayerGuard = true;
+			enemyMove();
+		}
+	});
+}
+
+function buttonTerminators() {
+	fightTest.off('click');
+	guardTest.off('click');
 }
 
 updateMessage("Player health: "+playerHP+"	Enemy health: "+enemyHP);
 
+
 buttonInitialisers();
+
 
 //Player physical attack logic
 function playerAttack(){
-	enemyHP -= 20;
+	enemyHP -= playerDmg;
 	if(enemyHP <= 0){
 		alert("You win!")
 	}
@@ -40,13 +57,19 @@ function playerAttack(){
 }
 
 
-
 function enemyMove() {
-	fightTest.off('click');
-	playerHP -= 15;
+	buttonTerminators();
+	if(checkPlayerGuard){
+		enemyDmg = Math.ceil(enemyDmg * 0.7);
+	}else{
+		enemyDmg = 10;
+	}
+	playerHP -= enemyDmg;
+	checkPlayerGuard = false;
 	if(playerHP <= 0){
 		alert("You lose")
 	}
+	
 	updateMessage("Player health: "+playerHP+"	Enemy health: "+enemyHP);
 	console.log("Player health:"+playerHP);
 	buttonInitialisers();
